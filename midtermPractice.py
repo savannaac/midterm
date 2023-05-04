36fba86ffccd62838f67e7b81418579d2496d308
savanna chiang <savannaac@gmail.com>
2023-05-04 01:00:28 -0500

    works through first problem

--- midtermPractice.py
+++ midtermPractice.py
@@ -1,4 +1,21 @@
+import re
 
+# numCapitalized
+# function that accepts sentence of words and spaces (no punctuation) 
+# and returns number of words in the sentence that are capitalized.
+# Note: NOT the total number of capitals in the string
+def numCapitalized(text):
+    no_punct = re.sub(r'[^\w\s]','',text)
+    words_list = no_punct.split()
+
+    return len([word for word in words_list if word[0].isupper()])
+
+    # returns total number of capitalized characters
+    # count = 0 
+    # for word in no_punct:
+    #     if word[0].isupper():
+    #         count += 1
+    # return count
 
 # exec(open('midtermPractice.py').read()) 
 if __name__=='__main__':
